9940e797d8dfdb3c980bcb43f86fcbdad56f5d0b
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2026-04-22 17:31:07 +0600

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/icon.ico',
+        type: 'image/x-icon',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/apple-touch-icon.png',
   },
 }
 
